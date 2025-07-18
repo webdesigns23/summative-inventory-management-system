75e9ac113cfe9e7dca0f008739cb035c6eb72762
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-07-18 16:27:40 -0600

    added html for Update and fixed renderCurrentInventory function to display details

--- client/scrips.js
+++ client/scrips.js
@@ -1,7 +1,7 @@
 fetch("http://127.0.0.1:5000/inventory")
   .then(response => response.json())
   .then(events => {
-    events.forEach(renderItem);
+    events.forEach(renderCurrentInventory);
   });
 
 document.querySelector("form").addEventListener("submit", (e) => {
@@ -18,12 +18,30 @@ document.querySelector("form").addEventListener("submit", (e) => {
     body: JSON.stringify({name, brand, price, stock})
   })
   .then(response => response.json())
-  .then(renderItem);
+  .then(renderCurrentInventory);
 });
 
-function renderItem(item) {
-//   console.log(item)
-  const li = document.createElement("li");
-  li.textContent = `${item.name}`;
-  document.querySelector("#item-list").appendChild(li);
+function renderCurrentInventory(item) {
+	const li = document.createElement("li");
+
+	const nameh3 = document.createElement("h3");
+  	nameh3.textContent = `${item.name}`;
+  	
+  	const brandSpan = document.createElement("span");
+  	brandSpan.textContent = `Brand: ${item.brand}`;
+  
+  	const priceSpan = document.createElement("span");
+  	priceSpan.textContent = `Price: $${item.price}`;
+
+  	const stockSpan = document.createElement("span");
+  	stockSpan.textContent = `Stock: ${item.stock}`;
+ 
+	li.appendChild(nameh3);
+  	li.appendChild(brandSpan);
+  	li.appendChild(document.createElement("br"));
+  	li.appendChild(priceSpan);
+  	li.appendChild(document.createElement("br"));
+  	li.appendChild(stockSpan);
+ 
+  	document.querySelector("#item-list").append(li);
 }
